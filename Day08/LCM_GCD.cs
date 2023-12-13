88b5990120b9612959e13d6cf9570904ce58e653
glhillman <glhillman@gmail.com>
2023-12-13 11:13:17 -0500

    Day 08

---
+++ Day08/LCM_GCD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08
+{
+    public class LCM_GCD
+    {
+        // Method to calculate the GCD (Greatest Common Divisor)
+        public static long GCD(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        // Method to calculate the LCM (Least Common Multiple) of two numbers
+        public static long LCM(long a, long b)
+        {
+            return (a * b) / GCD(a, b);
+        }
+
+        // Method to calculate the LCM of an array of numbers
+        public static long LCMOfArray(long[] arr)
+        {
+            long result = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                result = LCM(result, arr[i]);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Day08/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day08;
+using System;
+
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+internal class DayClass
+{
+    char[] _steps;
+    Dictionary<string, (string, string)> _maps = new();
+    public DayClass()
+    {
+        LoadData();
+    }
+
+    public void Part1()
+    {
+        int index = 0;
+        int wrap = _steps.Length;
+        int count = 0;
+        string key = "AAA";
+        while (key != "ZZZ")
+        {
+            (string left, string right) = _maps[key];
+            key = _steps[index] == 'L' ? left : right;
+            index = (index + 1) % wrap;
+            count++;
+        }
+        Console.WriteLine("Part1: {0}", count);
+    }
+
+    public void Part2()
+    {
+        List<string> set = new List<string>();
+        int index = 0;
+        long count = 0;
+        int wrap = _steps.Length;
+
+        foreach (string key in _maps.Keys)
+        {
+            if (key.EndsWith('A'))
+            {
+                set.Add(key);
+            }
+        }
+        List<long> zList = new();
+        foreach (string key in set)
+        {
+            string aKey = key;
+            index = 0;
+            count = 0;
+            while (aKey.EndsWith('Z') == false)
+            {
+                (string left, string right) = _maps[aKey];
+                aKey = _steps[index] == 'L' ? left : right;
+                index = (index + 1) % wrap;
+                count++;
+            }
+            zList.Add(count);
+        }
+        long lcm = LCM_GCD.LCMOfArray(zList.ToArray());
+        Console.WriteLine("Part2: {0}", lcm);
+    }
+
+    private void LoadData()
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+        if (File.Exists(inputFile))
+        {
+            string? line;
+            StreamReader file = new StreamReader(inputFile);
+            line = file.ReadLine();
+            _steps = line.ToArray();
+            file.ReadLine();
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] parts = line.Split(' ', '=', '(', ')', ',');
+                _maps[parts[0]] = (parts[4], parts[6]);
+            }
+
+            file.Close();
+        }
+    }
+
+}
